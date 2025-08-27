4531add318e8fd520f31280cb7d7fbe1a5e62828
rlaban12 <rlaeogus9786@gmail.com>
2025-08-27 15:33:53 +0900

    20250827_1533 commit

--- src/components/Todos/TodoInput.jsx
+++ src/components/Todos/TodoInput.jsx
@@ -15,9 +15,18 @@ const TodoInput = () => {
     return (
         <>
             <div className={wrapper}>
-                <form className={insertForm}>
+                <form className={insertForm}
+                      onSubmit={e => {
+                          e.preventDefault()
+                          const form = e.currentTarget; // form 요소
+                          const value = new FormData(form).get('todo'); // name="todo" 값
+                          console.log(value);
+
+                      }}
+                >
                     <input
                         type='text'
+                        name="todo" // 값을 가져오기 위한 name 추가
                         placeholder='할 일을 입력 후, 엔터를 누르세요!'
                     />
                 </form>
--- src/components/Todos/TodoItem.jsx
+++ src/components/Todos/TodoItem.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { MdDelete, MdDone } from 'react-icons/md';
 
 import styles from './scss/TodoItem.module.scss';
 
 const TodoItem = () => {
 
-    const { text, remove, 'todo-list-item': itemStyle, 'check-circle': checkCircle } = styles;
+    console.log(styles)
+
+    const { text, remove, 'todo-list-item': itemStyle, 'check-circle': checkCircle, active: checkActive } = styles;
+
+    const [ flag, setFlag ] = useState(true);
 
     return (
         <li className={itemStyle}>
-            <div className={checkCircle}>
-                <MdDone />
+            <div className={`${checkCircle} ${flag ? checkActive: ''}`} onClick={() => setFlag(!flag)} >
+                {flag && <MdDone/>}
             </div>
             <span className={text}>할 일 어쩌구~~</span>
             <div className={remove}>
